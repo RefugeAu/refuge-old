assistancechat/assistance

installing packages with -e

assistance/__init__.py
@@ -1,1 +1,3 @@
+"""Assistance package"""
+
 from ._version import __version__

RefugeAu/refuge-old

refuge/__init__.py
@@ -14,4 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Refuge package"""
+
 from ._version import __version__