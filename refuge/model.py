85a4e8c85fe9d08de345dd79c10b9f9c185bc72a
Simon Biggs <me@simonbiggs.net>
2023-05-03 17:58:08 +1000

    make soft_tokens more flexible

--- refuge/model.py
+++ refuge/model.py
@@ -57,10 +57,62 @@ class GPTNeoXPromptTuningLM(GPTNeoXForCausalLM):
     def generate(self, *args, **kwargs):
         return super().generate(*args, **kwargs)
 
-    # TODO: Add custom tokens here, also add custom tokens to the tokenizer
     def convert_token_to_embeddings(self, tokens: torch.Tensor) -> torch.Tensor:
+        soft_token_mask = tokens >= self.config.vocab_size
+        masked_tokens = tokens.clone()
+        masked_tokens[soft_token_mask] = 0
+
         embedding_layer = cast(nn.Embedding, self.gpt_neox.embed_in)
-        return embedding_layer(tokens)
+        embedding = embedding_layer(masked_tokens)
+
+        soft_token_idx = tokens[soft_token_mask] - self.config.vocab_size
+
+        soft_embedding = self.soft_prompt_embeddings[soft_token_idx]
+        embedding[soft_token_mask] = soft_embedding
+
+        return embedding
 
     def convert_logits_to_tokens(self, logits: torch.Tensor) -> torch.Tensor:
         return logits.argmax(dim=-1)
+
+    def prepare_inputs_for_generation(
+        self,
+        input_ids: torch.Tensor,
+        past_key_values=None,
+        attention_mask=None,
+        **kwargs
+    ):
+        # cut decoder_input_ids if past is used
+        if past_key_values and past_key_values[0] is not None:
+            input_ids = input_ids[:, -1:]
+
+        position_ids = kwargs.get("position_ids", None)
+        if attention_mask is not None and position_ids is None:
+            # create position_ids on the fly for batch generation
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
+            if past_key_values:
+                position_ids = position_ids[:, -1].unsqueeze(-1)
+
+        # if model is used as a decoder in encoder-decoder model, the decoder attention mask is created on the fly
+        if attention_mask is None:
+            input_shape = input_ids.shape
+            attention_mask = input_ids.new_ones(input_shape)
+
+        model_inputs: dict[str, torch.Tensor | None] = {}
+
+        try:
+            inputs_embeds = self.convert_token_to_embeddings(input_ids)
+            model_inputs = {"inputs_embeds": inputs_embeds}
+        except AssertionError:
+            model_inputs = {"input_ids": input_ids}
+
+        model_inputs.update(
+            {
+                "attention_mask": attention_mask,
+                "position_ids": position_ids,
+                "past_key_values": past_key_values,
+            }
+        )
+
+        return model_inputs
